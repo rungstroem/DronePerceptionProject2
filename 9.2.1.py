43a557d01de5a0f11f615adadc93e5978d2dbc30
Manumichmich78 <79519719+Manumichmich78@users.noreply.github.com>
2021-04-20 14:37:21 +0200

    Update 9.2.1.py

--- 9.2.1.py
+++ 9.2.1.py
@@ -4,19 +4,20 @@ import cv2
 import numpy as np
 
 def main():
-    img = cv2.imread("the_image_you_need.jpg")
+    img = cv2.imread("test.jpeg")
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     sift = cv2.SIFT_create()
-    kp = sift.detect(gray)
+    kp, des = sift.detectAndCompute(gray, None)
 
-    img=cv2.drawKeypoints(img, kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.imwrite("output/ex01_image_with_sift_features.png", img)
+    kp_img = cv2.drawKeypoints(img, kp, None, color=(0, 255, 0),
+            flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    cv2.imwrite('out_image.png', kp_img)
 
 
 main()
-
 
 
 
