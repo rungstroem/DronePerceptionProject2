15f7414919ecc9212705d7cddadf2cde11719281
KennethRLarsen <kenla16@student.sdu.dk>
2021-04-21 16:56:41 +0200

    Finally fixed the UTM converting

--- main.py
+++
@@ -1,8 +0,0 @@
-from utm import utmconv
-from math import pi,cos,acos,sin,atan2,sqrt
-
-f = open("./data/FlightRecord.csv", 'r');
-ff = open("./UTM_positions.csv", 'w');
-
-for line in f:
-    csv.line.split(",");
